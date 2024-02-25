924e0e20c827e373ec3a50721d07d026a4c6ca66
Emil Hristov <53617178+emodavitkov@users.noreply.github.com>
2024-02-25 15:13:13 +0100

    declare RestController paging sorting

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -3,6 +3,8 @@ package com.example.ec.repo;
 import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 
 /**
  * Tour Repository Interface
@@ -10,4 +12,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by Mary Ellen Bowman
  */
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(String code);
 }
